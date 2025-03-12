564f61fb3c18f51b9130552c8dd824d60c0f1aab
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2025-03-13 03:58:22 +0530

    Delete cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -44,8 +45,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products=user.cart.items
+      console.log(user.cart.items)
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
@@ -69,7 +72,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -77,9 +80,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .then(user => {
+      console.log(user.cart.items)
+      const products=user.cart.items.map(i =>{
+        return {quantity : i.quantity,product:{...i.productId._doc}}
+      })
+      const order = new Order({
+        user: {
+          name : req.user.name,
+          userId : req.user
+        },
+         products:products
+      })
+      return order.save()
+    })
     .then(result => {
       res.redirect('/orders');
     })
--- models/user.js
+++ models/user.js
@@ -19,6 +19,47 @@ const userSchema= new Schema({
   }
 })
 
+userSchema.methods.addToCart= function (product) {
+  //Ensure cart.items is an array, in case it's undefined
+    if (!Array.isArray(this.cart.items)) {
+      this.cart.items = [];
+    }
+
+    // Find the index of the product in the cart
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+         productId: product._id ,
+          quantity: newQuantity
+         });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    this.cart = updatedCart
+    return this.save()
+}
+
+userSchema.methods.removeFromCart = function (productId){
+  const updatedCartItems = this.cart.items.filter(item => {
+          return item.productId.toString() !== productId.toString()
+  })
+  this.cart.items = updatedCartItems
+  return this.save()
+}
+
+
+
 module.exports=mongoose.model('User',userSchema)
 
 
